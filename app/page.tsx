bbda3e1a8ff332e4c2d72871310f89ac4176e77c
NAO <nao@NAOnoMacBook-Air.local>
2023-06-26 12:50:18 +0900

    ディレクトリの修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import 'tailwindcss/tailwind.css';
+import { Modal } from '@/components/modal';
 
-export default function Home() {
-  return (
-    <>
-    <p>HELLO WORLD</p>
-    <Link href={'/index'}>BUTTON</Link>
-    </>
-  )
+export default function index() {
+
+    return(
+        <>
+        <div className='flex justify-center items-center h-full relative bg px-4'>
+        <div className='positionCenter popAnimation'>
+        <Modal isFillButton={true} isOutlineButton={true} isTextButton={true} 
+         src={'/'} height={'0'} width={'0'} 
+         heading={"Wel Come!"}
+         subtext={"こんにちは！あなたは高椋将平さんですか？"}
+         fillText={"はい。そうです！"} fillLink={"/"}
+         outlineText={"いいえ。違います！"} outlineLink={"/Error"}
+         textOnlyText={"わからない・・・"} textOnlyLink={"/"}
+         />
+        </div>
+        </div>
+        </>
+    )
 }
